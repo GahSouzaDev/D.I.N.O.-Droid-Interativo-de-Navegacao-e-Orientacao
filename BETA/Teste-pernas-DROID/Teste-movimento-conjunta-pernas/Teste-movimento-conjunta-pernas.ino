feb72254dc3fd3bf6006301ee684e95b82709157
GahSouzaDEV <emerdemolidor@gmail.com>
2024-10-25 14:03:57 -0300

    docs

--- BETA/Teste-pernas-DROID/Teste-movimento-conjunta-pernas/Teste-movimento-conjunta-pernas.ino
+++ BETA/Teste-pernas-DROID/Teste-movimento-conjunta-pernas/Teste-movimento-conjunta-pernas.ino
@@ -5,22 +5,54 @@
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 // Define os limites para os servos da perna direita (9 e 8)
-#define S9_MAX 118
-#define S9_MED 133
-#define S9_MIN 148
+#define S9_MAX 110
+#define S9_MED 130
+#define S9_MIN 150
 
-#define S8_MAX 108
-#define S8_MED 123
-#define S8_MIN 138
+#define S8_MAX 110
+#define S8_MED 130
+#define S8_MIN 150
 
 // Define os limites para os servos da perna esquerda (11 e 10)
-#define S11_MAX 100
-#define S11_MED 85
-#define S11_MIN 70
+#define S11_MAX 90
+#define S11_MED 70
+#define S11_MIN 50
 
-#define S10_MAX 90
-#define S10_MED 75
-#define S10_MIN 60
+#define S10_MAX 80
+#define S10_MED 60
+#define S10_MIN 40
+
+#define S13_MAX 100
+#define S13_MED 70
+#define S13_MIN 40
+
+#define S12_MAX 90
+#define S12_MED 60
+#define S12_MIN 30
+
+#define S15_MAX 95
+#define S15_MED 125
+#define S15_MIN 155
+
+#define S14_MAX 85
+#define S14_MED 115
+#define S14_MIN 145
+
+#define S4_MAX 120
+#define S4_MED 90
+#define S4_MIN 60
+
+#define S5_MAX 60
+#define S5_MED 90
+#define S5_MIN 120
+
+#define S6_MAX 120
+#define S6_MED 90
+#define S6_MIN 60
+
+#define S7_MAX 75
+#define S7_MED 105
+#define S7_MIN 135
 
 void setup() {
   Serial.begin(9600);
@@ -39,6 +71,10 @@ void loop() {
         pwm.setPWM(8, 0, pulseLength(S8_MAX));
         pwm.setPWM(11, 0, pulseLength(S11_MAX));
         pwm.setPWM(10, 0, pulseLength(S10_MAX));
+        pwm.setPWM(13, 0, pulseLength(S13_MAX));
+        pwm.setPWM(12, 0, pulseLength(S12_MAX));
+        pwm.setPWM(15, 0, pulseLength(S15_MAX));
+        pwm.setPWM(14, 0, pulseLength(S14_MAX));
         break;
 
       case '2':
@@ -47,6 +83,10 @@ void loop() {
         pwm.setPWM(8, 0, pulseLength(S8_MED));
         pwm.setPWM(11, 0, pulseLength(S11_MED));
         pwm.setPWM(10, 0, pulseLength(S10_MED));
+        pwm.setPWM(13, 0, pulseLength(S13_MED));
+        pwm.setPWM(12, 0, pulseLength(S12_MED));
+        pwm.setPWM(15, 0, pulseLength(S15_MED));
+        pwm.setPWM(14, 0, pulseLength(S14_MED));
         break;
 
       case '3':
@@ -55,6 +95,11 @@ void loop() {
         pwm.setPWM(8, 0, pulseLength(S8_MIN));
         pwm.setPWM(11, 0, pulseLength(S11_MIN));
         pwm.setPWM(10, 0, pulseLength(S10_MIN));
+        pwm.setPWM(13, 0, pulseLength(S13_MIN));
+        pwm.setPWM(12, 0, pulseLength(S12_MIN));
+        pwm.setPWM(15, 0, pulseLength(S15_MIN));
+        pwm.setPWM(14, 0, pulseLength(S14_MIN));
+        break;        
         break;
 
       case '4':
@@ -63,6 +108,10 @@ void loop() {
         pwm.setPWM(10, 0, pulseLength(S10_MAX));
         pwm.setPWM(9, 0, pulseLength(S9_MED));
         pwm.setPWM(8, 0, pulseLength(S8_MED));
+        pwm.setPWM(13, 0, pulseLength(S13_MED));
+        pwm.setPWM(12, 0, pulseLength(S12_MED));
+        pwm.setPWM(15, 0, pulseLength(S15_MAX));
+        pwm.setPWM(14, 0, pulseLength(S14_MAX));
         break;
 
       case '5':
@@ -71,6 +120,10 @@ void loop() {
         pwm.setPWM(8, 0, pulseLength(S8_MAX));
         pwm.setPWM(11, 0, pulseLength(S11_MED));
         pwm.setPWM(10, 0, pulseLength(S10_MED));
+        pwm.setPWM(13, 0, pulseLength(S13_MAX));
+        pwm.setPWM(12, 0, pulseLength(S12_MAX));
+        pwm.setPWM(15, 0, pulseLength(S15_MED));
+        pwm.setPWM(14, 0, pulseLength(S14_MED));
         break;
 
       case '6':
@@ -79,6 +132,10 @@ void loop() {
         pwm.setPWM(10, 0, pulseLength(S10_MIN));
         pwm.setPWM(9, 0, pulseLength(S9_MAX));
         pwm.setPWM(8, 0, pulseLength(S8_MAX));
+        pwm.setPWM(13, 0, pulseLength(S13_MAX));
+        pwm.setPWM(12, 0, pulseLength(S12_MAX));
+        pwm.setPWM(15, 0, pulseLength(S15_MIN));
+        pwm.setPWM(14, 0, pulseLength(S14_MIN));
         break;
 
       case '7':
@@ -87,6 +144,34 @@ void loop() {
         pwm.setPWM(8, 0, pulseLength(S8_MIN));
         pwm.setPWM(11, 0, pulseLength(S11_MAX));
         pwm.setPWM(10, 0, pulseLength(S10_MAX));
+        pwm.setPWM(13, 0, pulseLength(S13_MIN));
+        pwm.setPWM(12, 0, pulseLength(S12_MIN));
+        pwm.setPWM(15, 0, pulseLength(S15_MAX));
+        pwm.setPWM(14, 0, pulseLength(S14_MAX));
+        break;
+
+        case '8':
+        // Coloca a perna direita no mínimo e a perna esquerda no máximo
+        pwm.setPWM(4, 0, pulseLength(S5_MIN));
+        pwm.setPWM(5, 0, pulseLength(S6_MIN));
+        pwm.setPWM(6, 0, pulseLength(S7_MAX));
+        pwm.setPWM(7, 0, pulseLength(S8_MAX));        
+        break;
+
+        case '9':
+        // Coloca a perna direita no mínimo e a perna esquerda no máximo
+        pwm.setPWM(4, 0, pulseLength(S4_MAX));
+        pwm.setPWM(5, 0, pulseLength(S5_MAX));
+        pwm.setPWM(6, 0, pulseLength(S6_MIN));
+        pwm.setPWM(7, 0, pulseLength(S7_MIN));        
+        break;
+
+        case '0':
+        // Coloca a perna direita no mínimo e a perna esquerda no máximo
+        pwm.setPWM(4, 0, pulseLength(S4_MIN));
+        pwm.setPWM(5, 0, pulseLength(S5_MIN));
+        pwm.setPWM(6, 0, pulseLength(S6_MAX));
+        pwm.setPWM(7, 0, pulseLength(S7_MAX));        
         break;
 
       default:
