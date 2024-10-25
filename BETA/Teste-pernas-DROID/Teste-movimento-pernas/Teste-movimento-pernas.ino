028185e1862f86c3dc2080464560d2542c70b815
GahSouzaDEV <emerdemolidor@gmail.com>
2024-10-25 08:35:33 -0300

    docs

--- BETA/Teste-pernas-DROID/Teste-movimento-pernas/Teste-movimento-pernas.ino
+++ BETA/Teste-pernas-DROID/Teste-movimento-pernas/Teste-movimento-pernas.ino
@@ -5,21 +5,21 @@
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 // Define os limites para os servos
-#define S4_MIN 65
+#define S4_MIN 60
 #define S4_MED 90
-#define S4_MAX 105
+#define S4_MAX 120
 
-#define S5_MIN 115
+#define S5_MIN 120
 #define S5_MED 90
-#define S5_MAX 85
+#define S5_MAX 60
 
-#define S6_MIN 65
+#define S6_MIN 60
 #define S6_MED 90
-#define S6_MAX 105
+#define S6_MAX 120
 
-#define S7_MIN 130
+#define S7_MIN 135
 #define S7_MED 105
-#define S7_MAX 90
+#define S7_MAX 75
 
 void setup() {
   Serial.begin(9600);
@@ -57,6 +57,24 @@ void loop() {
         // Coloca os servos de rotação na posição média
         pwm.setPWM(6, 0, pulseLength(S6_MAX));
         pwm.setPWM(7, 0, pulseLength(S7_MAX));
+        break;
+
+         case '4':
+        // Coloca os servos laterais na posição máxima
+        pwm.setPWM(4, 0, pulseLength(S4_MIN));
+        pwm.setPWM(5, 0, pulseLength(S5_MIN));
+        // Coloca os servos de rotação na posição média
+        pwm.setPWM(6, 0, pulseLength(S6_MAX));
+        pwm.setPWM(7, 0, pulseLength(S7_MAX));
+        break;
+
+         case '5':
+        // Coloca os servos laterais na posição máxima
+        pwm.setPWM(4, 0, pulseLength(S4_MAX));
+        pwm.setPWM(5, 0, pulseLength(S5_MAX));
+        // Coloca os servos de rotação na posição média
+        pwm.setPWM(6, 0, pulseLength(S6_MIN));
+        pwm.setPWM(7, 0, pulseLength(S7_MIN));
         break;
 
       default:
