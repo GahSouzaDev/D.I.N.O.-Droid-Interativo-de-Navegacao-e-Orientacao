a0945ee124dc787aad6aa4e7f0f2058434d16a5b
GahSouzaDEV <emerdemolidor@gmail.com>
2024-11-02 06:38:55 -0300

    fix

--- D.I.N.O/Codigos/posicao-inicial/posicao-inicial.ino
+++ D.I.N.O/Codigos/posicao-inicial/posicao-inicial.ino
@@ -8,9 +8,9 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define SERVO_MAX_PULSE 512 // Pulso máximo para o servo
 
 int minPosition[16] = {60, 5, 90, 40,60,120,60,135,135,135,50,60,70,80,105,115};       // Posições mínimas
-int initialPosition[16] = {110, 100, 120, 75,90,90,90,105,120,120,65,75,80,90,95,105}; // Posições iniciais
+int initialPosition[16] = {130, 100, 120, 75,90,90,90,105,120,120,65,75,80,90,95,105}; // Posições iniciais
 int maxPosition[16] = {160, 195, 150, 130,120,60,120,75,105,105,80,90,90,100,85,95};   // Posições máximas
-int currentPosition[16] = {110, 100, 120, 75,90,90,90,105,120,120,65,75,80,90,95,105}; // Posições atuais
+int currentPosition[16] = {130, 100, 120, 75,90,90,90,105,120,120,65,75,80,90,95,105}; // Posições atuais
 
 // Definição dos pinos
 #define LED_ESP32 2       // LED integrado no ESP32
