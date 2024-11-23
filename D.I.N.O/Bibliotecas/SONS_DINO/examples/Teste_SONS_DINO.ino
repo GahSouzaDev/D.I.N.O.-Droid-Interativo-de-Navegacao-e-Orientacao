5d423fbef2f8eaf03d7a4430c664a4774e5f455a
GahSouzaDEV <emerdemolidor@gmail.com>
2024-11-23 13:40:17 -0300

    fix

--- D.I.N.O/Bibliotecas/SONS_DINO/examples/Teste_SONS_DINO.ino
+++ D.I.N.O/Bibliotecas/SONS_DINO/examples/Teste_SONS_DINO.ino
@@ -1,26 +1,49 @@
-#include <SONS_DINO.h>
+#include "SONS_DINO.h"
 
-// Define o pino do buzzer e o volume base
-SONS_DINO dino(13, 25);
+#define BUZZER_PIN 13
+#define VOLUME_BASE 25
+
+SONS_DINO dino(BUZZER_PIN, VOLUME_BASE);
 
 void setup() {
-    Serial.begin(9600); // Inicializa a comunicação serial
-    dino.begin();       // Inicializa o buzzer
+    Serial.begin(9600);
+    dino.begin(); // Inicializa o buzzer
 }
 
 void loop() {
     if (Serial.available() > 0) {
-        char comando = Serial.read(); // Lê o comando via Serial
+        char comando = Serial.read();
         switch (comando) {
-            case '1': dino.confirmacao(); break;
-            case '2': dino.falaNome(); break;
-            case '3': dino.positivo(); break;
-            case '4': dino.negativo(); break;
-            case '5': dino.duvida(); break;
-            case '6': dino.erro(); break;
-            case '7': dino.ajuda(); break;
-            case '8': dino.alerta(); break;
-            case '9': dino.contente(); break;
+            case '1':
+                dino.confirmacao();
+                break;
+            case '2':
+                dino.falaNome();
+                break;
+            case '3':
+                dino.positivo();
+                break;
+            case '4':
+                dino.negativo();
+                break;
+            case '5':
+                dino.duvida();
+                break;
+            case '6':
+                dino.erro();
+                break;
+            case '7':
+                dino.ajuda();
+                break;
+            case '8':
+                dino.alerta();
+                break;
+            case '9':
+                dino.contente();
+                break;
+            default:
+                Serial.println("Comando inválido!");
+                break;
         }
     }
 }
