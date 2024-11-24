4655a9e6724553e586630d8ea52730e1fb5f350d
GahSouzaDEV <emerdemolidor@gmail.com>
2024-11-24 11:54:28 -0300

    fix

--- D.I.N.O/Codigos/D.I.N.O/D.I.N.O.ino
+++ D.I.N.O/Codigos/D.I.N.O/D.I.N.O.ino
@@ -1,102 +1,33 @@
-#include <WiFi.h>
-#include <WebServer.h>
-
-// Definir o nome e a senha da rede Wi-Fi que o ESP32 irá criar
-const char *ssid = "D.I.N.O."; // Nome da rede Wi-Fi
-const char *password = "123456789";  // Senha da rede Wi-Fi
-
-// Criação do servidor web na porta 80
-WebServer server(80);
-
-// HTML da página que será servida
-const char* htmlContent = R"rawliteral(
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dino com Olhos Piscando</title>
-    <style>
-        body {
-            font-family: 'Courier New', Courier, monospace;
-            text-align: center;
-        }
-
-        /* Definir a animação de piscar (olhos fechando e abrindo) */
-        @keyframes piscar {
-            0%, 100% {
-                r: 10; /* Olhos abertos */
-            }
-            50% {
-                r: 0; /* Olhos fechados */
-            }
-        }
-
-        /* Aplicar a animação aos dois olhos ao mesmo tempo */
-        .olhos {
-            animation: piscar 2s infinite; /* Animação que repete indefinidamente */
-        }
-    </style>
-</head>
-<body>
-    <h1>Dino INICIO</h1>
-    <svg xmlns="http://www.w3.org/2000/svg" width="300" height="500" viewBox="0 0 300 500">
-        <!-- Pernas -->
-        <rect x="40" y="360" width="40" height="120" fill="gray" stroke="black" stroke-width="2" />
-        <rect x="220" y="360" width="40" height="120" fill="gray" stroke="black" stroke-width="2" />
-        
-        <!-- Corpo -->
-        <rect x="50" y="255" width="200" height="120" fill="silver" stroke="black" stroke-width="2" />
-        
-        <!-- Pescoço -->
-        <rect x="120" y="180" width="60" height="75" fill="gray" stroke="black" stroke-width="2" />
-        
-        <!-- Cabeça -->
-        <rect x="60" y="100" width="180" height="80" fill="silver" stroke="black" stroke-width="2" />
-        
-        <!-- Antenas -->
-        <rect x="50" y="10" width="10" height="100" fill="black" />
-        <rect x="240" y="10" width="10" height="100" fill="black" />
-             
-        <!-- Olhos -->
-        <circle cx="120" cy="140" r="15" fill="black"  />
-        <circle cx="180" cy="140" r="15" fill="black"  />
-        <circle cx="120" cy="140" r="12" fill="white" 
-        class="olhos"/>
-        <circle cx="180" cy="140" r="12" fill="white"
-        class="olhos" />
-
-        <!-- Pés -->
-        <rect x="30" y="475" width="60" height="20" fill="black" />
-        <rect x="210" y="475" width="60" height="20" fill="black" />
-    </svg>
-</body>
-</html>
-
-)rawliteral";
-
-// Função que será chamada quando a página for acessada
-void handleRoot() {
-  server.send(200, "text/html", htmlContent); // Envia o conteúdo HTML
-}
-
+configuracoes()
+         
 void setup() {
-  Serial.begin(115200);
   
-  // Inicializa o ESP32 como Access Point
-  WiFi.softAP(ssid, password);  // Cria a rede Wi-Fi
-  
-  Serial.println();
-  Serial.print("IP do ESP32: ");
-  Serial.println(WiFi.softAPIP()); // Mostra o IP para acessar a página
+  pwm.begin();
+  pwm.setPWMFreq(50); // Configura a frequência para 50Hz
 
-  // Define a função de resposta para a raiz
-  server.on("/", HTTP_GET, handleRoot);
-  
-  // Inicia o servidor
-  server.begin();
+  for (int i = 0; i < 16; i++) {
+    moveServo(i, initialPosition[i]);
+  }
+  // Configuração dos pinos como saídas
+  pinMode(LED_ESP32, OUTPUT);
+  pinMode(OLHO_ESQUERDO, OUTPUT);
+  pinMode(OLHO_DIREITO, OUTPUT);
+  pinMode(LED_VERDE, OUTPUT);
+  pinMode(LED_AMARELO, OUTPUT);
+  pinMode(LED_VERMELHO, OUTPUT);
+  pinMode(LED_AZUL, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+
+  // Sequência de LEDs coloridos
+  sequenciaLEDs();
+
+  bipBuzzer();
+
 }
 
 void loop() {
-  server.handleClient(); // Aguarda e processa as requisições HTTP
+
+  // Piscar olhos de forma natural
+  piscarOlhos();
+
 }
---
+++ D.I.N.O/Codigos/D.I.N.O/configuracoes.ino
@@ -0,0 +1,117 @@
+void configuracoes(){
+//D.I.N.O.
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+#define SERVO_MIN_PULSE 112 // Pulso mínimo para o servo
+#define SERVO_MAX_PULSE 512 // Pulso máximo para o servo
+
+//----CABEÇA----//
+//Pescoço
+int PescocoMin = 90, PescocoMed = 120, PescocoMax = 160;
+
+//Rotação da cabeça
+int RotacaoCabecaMed = 100; 
+//Rotação para DIREITA
+int RotacaoCabecaDirMax = 0, RotacaocabecaDirMed = 50;
+//Rotação para ESQUERDA
+int RotacaoCabecaEsqMax = 150, RotacaoCabecaEspMed = 200;
+
+//Antenas
+int AntenasMed = 120, AntenasMax = 150, AntenasMin = 90;
+
+//Elevação da cabeça
+int ElevacaoMed = 75;
+//Cabeça para BAIXO
+int ElevacaoBaixoMed = 57.5, ElevacaoBaixoMax = 40;
+//Cabeça para CIMA
+int ElevacaoCimaMed = 92.5, ElevacaoCimaMax = 130;
+
+//---PERNAS---//
+//          ESQUEDA                  DIREITA   
+
+//Rotação  
+//                        MEDIA   
+int RotacaoPernaEsqMed1 = 90, RotacaoPernaDirMed1 = 90;
+int RotacaoPernaEsqMed2 = 90, RotacaoPernaDirMed2 = 105;
+//                 Rotação para dentro
+int RotacaoPernaEsqMin1 = 60, RotacaoPernaDirMin1 = 60;
+int RotacaoPernaEsqMin2 = 120, RotacaoPernaDirMin2 = 135;
+//                  Rotação para fora
+int RotacaoPernaEsqMax1 = 120, RotacaoPernaDirMax1 = 120;
+int RotacaoPernaEsqMax2 = 60, RotacaoPernaDirMax2 = 75;
+
+//COXA (Parte superior da perna)
+//                        MEDIA   
+int MoveCoxaEsqMed1 = 120, MoveCoxaDirMed1 = 65;
+int MoveCoxaEsqMed2 = 120, MoveCoxaDirMed2 = 75;
+//             Rotação para dentro
+int MoveCoxaEsqMin1 = 135, MoveCoxaDirMin1 = 50;
+int MoveCoxaEsqMin2 = 135, MoveCoxaDirMin2 = 60;
+//              Rotação para fora
+int MoveCoxaEsqMax1 = 105, MoveCoxaDirMax1 = 80;
+int MoveCoxaEsqMax2 = 105, MoveCoxaDirMax2 = 90;
+
+//PANTURRILHA (Parte de baixo da perna)
+//                        MEDIA   
+int MovePantEsqMed1 = 95, MovePantDirMed1 = 90;
+int MovePantEsqMed2 = 105, MovePantDirMed2 = 90;
+//              Rotação para dentro
+int MovePantEsqMin1 = 105, MovePantDirMin1 = 70;
+int MovePantEsqMin2 = 115, MovePantDirMin2 = 80;
+//               Rotação para fora
+int MovePantEsqMax1 = 85, MovePantDirMax1 = 90;
+int MovePantEsqMax2 = 95, MovePantDirMax2 = 100;
+
+//Exemplo da sequencia a ser seguida!
+int initialPosition[16] =   
+{
+  PescocoMed, 
+  RotacaoCabecaMed, 
+  AntenasMed, 
+  ElevacaoMed,  
+  
+  RotacaoPernaDirMed1,RotacaoPernaDirMed2,
+  RotacaoPernaEsqMed1,RotacaoPernaEsqMed2,   
+
+  MoveCoxaEsqMax1, MoveCoxaEsqMax2,
+  MoveCoxaDirMax1, MoveCoxaDirMax2,
+
+  MovePantDirMax1, MovePantDirMax2,
+  MovePantEsqMax1, MovePantEsqMax2
+};
+
+int currentPosition[16] =   
+{
+  PescocoMed, 
+  RotacaoCabecaMed, 
+  AntenasMed, 
+  ElevacaoMed,  
+  
+  RotacaoPernaDirMed1,RotacaoPernaDirMed2,
+  RotacaoPernaEsqMed1,RotacaoPernaEsqMed2,   
+
+  MoveCoxaEsqMax1, MoveCoxaEsqMax2,
+  MoveCoxaDirMax1, MoveCoxaDirMax2,
+
+  MovePantDirMax1, MovePantDirMax2,
+  MovePantEsqMax1, MovePantEsqMax2
+};
+
+// Definição dos pinos
+#define LED_ESP32 2       // LED integrado no ESP32
+
+#define OLHO_ESQUERDO 14  // Olho esquerdo
+#define OLHO_DIREITO 12   // Olho direito
+// Define o brilho dos OLHOS (0 a 255)
+int brilho = 10; // Você pode ajustar isso conforme necessário
+
+#define LED_VERMELHO 32     // LED vermelho
+#define LED_AMARELO 26     // LED amarelo
+#define LED_VERDE 33      // LED verde
+#define LED_AZUL 27       // LED azul
+
+#define BUZZER 13         // Buzzer
+}
